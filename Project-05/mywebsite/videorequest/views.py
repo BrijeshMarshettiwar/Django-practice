b8b9ead5d0e327824b79735cb0dc8569a9c81468
Brijesh Marshettiwar <123184136+BrijeshMarshettiwar@users.noreply.github.com>
2023-11-04 18:42:56 -0400

    Update views.py

--- Project-05/mywebsite/videorequest/views.py
+++ Project-05/mywebsite/videorequest/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from . models import Video
+from . forms import VideoForm
 # Create your views here.
 def index(request):
-    return render(request, 'videorequest/index.html')
+    videos = Video.objects.order_by('-date_added')
+    context = {'videos': videos}
+    return render(request, 'videorequest/index.html', context)
 
 def vrform(request):
-    return render(request, 'vedeorequest/vrform.html')
+    if request.method == 'POST':
+        form = VideoForm(request.POST)
+
+        if form.is_valid():
+            new_req = Video(videotitle=request.POST['videoname'],videodes=request.POST['videodes'])
+            new_req.save()
+            return redirect('index')
+    else:
+        form = VideoForm()
+
+    context = {'form': form}
+
+
+
+
+    return render(request, 'videorequest/vrform.html', context)
