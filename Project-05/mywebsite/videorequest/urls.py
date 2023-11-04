a329bf8369ee62d07a771ff68a5e6c8f00b8ed96
Brijesh Marshettiwar <123184136+BrijeshMarshettiwar@users.noreply.github.com>
2023-11-04 18:42:54 -0400

    Update urls.py

--- Project-05/mywebsite/videorequest/urls.py
+++ Project-05/mywebsite/videorequest/urls.py
@@ -1,7 +1,6 @@
-
 from django.urls import path
-from . import views
+from . import views 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('vrform', views.vrform, name='vrform' ),
+    path('vrform/', views.vrform, name='vrform' ),
 ]
