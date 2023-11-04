41ba3ff0fca8c49693b54dde259063bed9e3c6e6
Brijesh Marshettiwar <123184136+BrijeshMarshettiwar@users.noreply.github.com>
2023-11-04 18:40:43 -0400

    Update models.py

--- Project-04/website/articles/models.py
+++ Project-04/website/articles/models.py
@@ -3,13 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Article(models.Model):
-    author = models.Foreignkey(
+    author = models.ForeignKey(
         'auth.User',
         on_delete=models.CASCADE,
     )
     title = models.CharField(max_length=200)
     text = models.TextField()
-
 
 
     def __str__(self):
